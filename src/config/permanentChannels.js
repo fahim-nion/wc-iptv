f47f92940e4cd98f527e1efedc6a720f1aca0b8e
Fahim Morshed <fahimalhassan4@gmail.com>
2026-09-19 16:52:05 +0600

    updated

--- src/config/permanentChannels.js
+++ src/config/permanentChannels.js
@@ -5,7 +5,7 @@ export const PERMANENT_CHANNELS = [
     name: 'BEIN SPORTS 1',
     logo: 'https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/international/beinsports/old/horizontal/bein-sports-1-hz-int.png',
     group: 'Sports',
-    url: 'https://andro.evrenesoglu99.click/checklist/batutest.m3u8',
+    url: 'https://andro.evrenesoglu107.click/checklist/batutest.m3u8',
     isPermanent: true
   },
   {
@@ -13,7 +13,7 @@ export const PERMANENT_CHANNELS = [
     name: 'BEIN SPORTS 2',
     logo: 'https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/international/beinsports/old/horizontal/bein-sports-2-hz-int.png',
     group: 'Sports',
-    url: 'https://andro.evrenesoglu99.click/checklist/androstreamlivebs2.m3u8',
+    url: 'https://andro.evrenesoglu107.click/checklist/androstreamlivebs2.m3u8',
     isPermanent: true
   },
 
@@ -22,7 +22,7 @@ export const PERMANENT_CHANNELS = [
     name: 'BEIN SPORTS 3',
     logo: 'https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/international/beinsports/old/horizontal/bein-sports-3-hz-int.png',
     group: 'Sports',
-    url: 'https://andro.evrenesoglu99.click/checklist/androstreamlivebs3.m3u8',
+    url: 'https://andro.evrenesoglu107.click/checklist/androstreamlivebs3.m3u8',
     isPermanent: true
   },
   {
@@ -30,7 +30,7 @@ export const PERMANENT_CHANNELS = [
     name: 'BEIN SPORTS 4',
     logo: 'https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/international/beinsports/old/horizontal/bein-sports-4-hz-int.png',
     group: 'Sports',
-    url: 'https://andro.evrenesoglu99.click/checklist/androstreamlivebs4.m3u8',
+    url: 'https://andro.evrenesoglu107.click/checklist/androstreamlivebs4.m3u8',
     isPermanent: true
   },
   {
@@ -38,7 +38,7 @@ export const PERMANENT_CHANNELS = [
     name: 'BEIN SPORTS 5',
     logo: 'https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/international/beinsports/old/horizontal/bein-sports-5-hz-int.png',
     group: 'Sports',
-    url: 'https://andro.evrenesoglu99.click/checklist/androstreamlivebs5.m3u8',
+    url: 'https://andro.evrenesoglu107.click/checklist/androstreamlivebs5.m3u8',
     isPermanent: true
   },
   {
@@ -46,7 +46,15 @@ export const PERMANENT_CHANNELS = [
     name: 'BEIN SPORTS MAx 1',
     logo: 'https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/international/beinsports/old/horizontal/bein-sports-1-max-hz-int.png',
     group: 'Sports',
-    url: 'https://andro.evrenesoglu99.click/checklist/androstreamlivebsm1.m3u8',
+    url: 'https://andro.evrenesoglu107.click/checklist/androstreamlivebsm1.m3u8',
+    isPermanent: true
+  },
+  {
+    id: 'permanent-7',
+    name: 'BEIN SPORTS MAx 2',
+    logo: 'https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/international/beinsports/old/horizontal/bein-sports-2-max-hz-int.png',
+    group: 'Sports',
+    url: 'https://andro.evrenesoglu107.click/checklist/androstreamlivebsm2.m3u8',
     isPermanent: true
   },
   {
@@ -54,7 +62,7 @@ export const PERMANENT_CHANNELS = [
     name: 'S Sport',
     logo: 'https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/turkey/s-sport-tr.png',
     group: 'Sports',
-    url: 'https://andro.evrenesoglu99.click/checklist/androstreamlivess1.m3u8',
+    url: 'https://andro.evrenesoglu107.click/checklist/androstreamlivess1.m3u8',
     isPermanent: true
   },
   {
@@ -62,7 +70,7 @@ export const PERMANENT_CHANNELS = [
     name: 'S Sport 2',
     logo: 'https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/turkey/s-sport-2-tr.png',
     group: 'Sports',
-    url: 'https://andro.evrenesoglu99.click/checklist/androstreamlivess2.m3u8',
+    url: 'https://andro.evrenesoglu107.click/checklist/androstreamlivess2.m3u8',
     isPermanent: true
   },
   {
@@ -70,7 +78,7 @@ export const PERMANENT_CHANNELS = [
     name: 'Sport TV 3',
     logo: 'https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/portugal/sport-tv-3-pt.png',
     group: 'Sports',
-    url: 'https://andro.evrenesoglu99.click/checklist/androstreamlivessplus1.m3u8',
+    url: 'https://andro.evrenesoglu107.click/checklist/androstreamlivesm1.m3u8',
     isPermanent: true
   },
   {
@@ -78,15 +86,15 @@ export const PERMANENT_CHANNELS = [
     name: 'Smart Sport 1',
     logo: 'https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/turkey/spor-smart-hd-tr.png',
     group: 'Sports',
-    url: 'https://andro.evrenesoglu99.click/checklist/androstreamlivesm1.m3u8',
+    url: 'https://andro.evrenesoglu107.click/checklist/androstreamlivesm1.m3u8',
     isPermanent: true
   },
   {
     id: 'permanent-12',
-    name: 'Smart Sport 1',
+    name: 'Smart Sport 2',
     logo: 'https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/turkey/movie-smart-turk-hd-tr.png',
     group: 'Sports',
-    url: 'https://andro.evrenesoglu99.click/checklist/androstreamlivesm2.m3u8',
+    url: 'https://andro.evrenesoglu107.click/checklist/androstreamlivesm2.m3u8',
     isPermanent: true
   },
   {
@@ -94,7 +102,7 @@ export const PERMANENT_CHANNELS = [
     name: 'Euro Sport 1',
     logo: 'https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/spain/eurosport-1-es.png',
     group: 'Sports',
-    url: 'https://andro.evrenesoglu99.click/checklist/androstreamlivees1.m3u8',
+    url: 'https://andro.evrenesoglu107.click/checklist/androstreamlivees1.m3u8',
     isPermanent: true
   },
   {
@@ -102,7 +110,7 @@ export const PERMANENT_CHANNELS = [
     name: 'Euro Sport 2',
     logo: 'https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/spain/eurosport-2-hd-es.png',
     group: 'Sports',
-    url: 'https://andro.evrenesoglu99.click/checklist/androstreamlivees2.m3u8',
+    url: 'https://andro.evrenesoglu107.click/checklist/androstreamlivees2.m3u8',
     isPermanent: true
   },
   {
@@ -110,7 +118,7 @@ export const PERMANENT_CHANNELS = [
     name: 'TRT SPOR',
     logo: 'https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/turkey/trt-spor-tr.png',
     group: 'Sports',
-    url: 'https://andro.evrenesoglu99.click/checklist/androstreamlivetrts.m3u8',
+    url: 'https://andro.evrenesoglu107.click/checklist/androstreamlivetrts.m3u8',
     isPermanent: true
   },
   {
@@ -118,7 +126,7 @@ export const PERMANENT_CHANNELS = [
     name: 'A SPORT',
     logo: 'https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/turkey/a-spor-tr.png',
     group: 'Sports',
-    url: 'https://andro.evrenesoglu99.click/checklist/androstreamliveas.m3u8',
+    url: 'https://andro.evrenesoglu107.click/checklist/androstreamliveas.m3u8',
     isPermanent: true
   },
   {
@@ -126,7 +134,7 @@ export const PERMANENT_CHANNELS = [
     name: 'TEKRAR HT SPORT',
     logo: 'https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/turkey/ht-hayat-tr.png',
     group: 'Sports',
-    url: 'https://andro.evrenesoglu99.click/checklist/androstreamliveht.m3u8',
+    url: 'https://andro.evrenesoglu107.click/checklist/androstreamliveht.m3u8',
     isPermanent: true
   },
   {
@@ -134,7 +142,7 @@ export const PERMANENT_CHANNELS = [
     name: 'FB SPORT',
     logo: 'https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/misc/media/fubo-tv.png',
     group: 'Sports',
-    url: 'https://andro.evrenesoglu99.click/checklist/androstreamlivefb.m3u8',
+    url: 'https://andro.evrenesoglu107.click/checklist/androstreamlivefb.m3u8',
     isPermanent: true
   },
   {
@@ -142,7 +150,7 @@ export const PERMANENT_CHANNELS = [
     name: 'TRT SPORT',
     logo: 'https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/turkey/trt-spor-tr.png',
     group: 'Sports',
-    url: 'https://andro.evrenesoglu99.click/checklist/androstreamliveexn3.m3u8',
+    url: 'https://andro.evrenesoglu107.click/checklist/androstreamliveexn3.m3u8',
     isPermanent: true
   },
   {
@@ -150,7 +158,7 @@ export const PERMANENT_CHANNELS = [
     name: 'CITY SPORT',
     logo: 'https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/bulgaria/city-tv-bg.png',
     group: 'Sports',
-    url: 'https://andro.evrenesoglu99.click/checklist/androstreamliveexn5.m3u8',
+    url: 'https://andro.evrenesoglu107.click/checklist/androstreamliveexn5.m3u8',
     isPermanent: true
   },
   {
@@ -158,7 +166,7 @@ export const PERMANENT_CHANNELS = [
     name: 'T SPORT',
     logo: 'https://raw.githubusercontent.com/tv-logo/tv-logos/refs/heads/main/countries/turkey/24-tr.png',
     group: 'Sports',
-    url: 'https://andro.evrenesoglu99.click/checklist/androstreamliveexn6.m3u8',
+    url: 'https://andro.evrenesoglu107.click/checklist/androstreamliveexn6.m3u8',
     isPermanent: true
   },
 ];
